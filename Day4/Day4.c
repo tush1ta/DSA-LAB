589e2a7be0311e6ab1e14ad8380317602c835a77
tush1ta <tushitasharma833@gmail.com>
2026-02-05 21:37:47 +0530

    .

---
+++ Day4/Day4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int start = 0, end = n - 1;
+
+    while (start < end) {
+  
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+ 
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
